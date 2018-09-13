99bdbf18ba3dc24744c798c397880210e2d30b0d
Linesh <linesh.simpcity@gmail.com>
2018-09-14 00:06:04 +0800

    ✨feature: introduce shelljs

--- TODOLIST.md
+++ TODOLIST.md
@@ -2,4 +2,6 @@
 
 * [x] be able to run command locally - 7min
 * [ ] be able to generate a project structure - 25min
+  * [ ] create a new project 
+  * [ ] init with 'git init' if 'git' command is present
 * [ ] be able to take in parameters - 10min
--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 
-console.log('hello')
+const shell = require('shelljs')
+
+if (!shell.which('git')) {
+  console.log('command not found: git. skipping git init...')
+}
 
 const [nodePath, commandName, ...args] = process.argv
 console.log(nodePath, commandName, args)
