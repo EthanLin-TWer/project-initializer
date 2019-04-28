57bcccc849369d84ec40f36f5841c7cdc3f8a25e
Linesh <linesh.simpcity@gmail.com>
2019-04-28 13:13:00 +0800

    🔧chore: update babel dependencies

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+export class Calculator {
+  constructor(number) {
+    this.number = number
+  }
+
+  add(one) {
+    return this.number + one
+  }
+}
--- src/cli.test.js
+++ src/cli.test.js
@@ -1,3 +1,5 @@
+import { Calculator } from './cli'
+
 it('should return 2 when calculating 1 + 1', () => {
-  expect(1 + 1).toEqual(2)
+  expect(new Calculator(1).add(1)).toEqual(2)
 })
