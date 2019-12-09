5a6c78dd158699f1dac1d9af1f13945666a029e8
Linesh <linesh.simpcity@gmail.com>
2019-12-10 00:43:40 +0800

    🔧chore: introduce typescript for current project

--- src/cli.test.js
+++ src/cli.test.js
@@ -1,9 +1,24 @@
-import { Calculator } from './cli'
+import { TypeScriptCalculator } from './cli'
+
+class JavaScriptCalculator {
+  #number
+  constructor(number) {
+    this.#number = number
+  }
+
+  add(number) {
+    return this.#number + number
+  }
+}
 
 it('should ES6 import/export works', () => {
   expect(1 + 1).toEqual(2)
 })
 
-it('should class private field syntax works', () => {
-  expect(new Calculator(1).add(1)).toEqual(2)
+it('should typescript works', () => {
+  expect(new TypeScriptCalculator(1).add(1)).toEqual(2)
+})
+
+it('should class private field syntax works for .js files', () => {
+  expect(new JavaScriptCalculator(1).add(1)).toEqual(2)
 })
---
+++ src/cli.ts
@@ -0,0 +1,10 @@
+export class TypeScriptCalculator {
+  private readonly number: number
+  constructor(number) {
+    this.number = number
+  }
+
+  add(one): string {
+    return this.number + one
+  }
+}
